c29a95a057195a80ad84377d6711b994a726c9e6
jfinlays <jfinlays@jfinlayss-MacBook-Pro.local>
2017-02-23 10:02:27 +0100

    change yes to maybe

--- widgets/isxup/isxup.js
+++ widgets/isxup/isxup.js
@@ -14,7 +14,7 @@ class isxup extends Widget {
         return (
             <div className={`${this.props.className} ${this.status()}`}>
                 <h1 className="title">Is CBLCA Up?</h1>
-                <h3>{this.state.isUp ? "Yes" : "No" }</h3>
+                <h3>{this.state.isUp ? "Maybe" : "No" }</h3>
                 <div className="container">
                 <p className="updated-at">{this.updatedAt(this.state.updated_at)}</p>
                 <p className="right-block">Minutes Up: {this.state.minutesUp}</p>
